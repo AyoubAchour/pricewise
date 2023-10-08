d4a2aad7946ba3d042927522d9d9d569a5102e6a
AyoubAchour <medayoubachour69@gmail.com>
2023-10-08 18:51:58 +0100

    first commit

---
+++ lib/mongoose.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+let isConnected = false;
+
+export const connectToDB = async () => {
+    mongoose.set('strictQuery', true);
+
+    if (!process.env.MONGODB_URI) return console.log('MONGODB_URI is not defined');
+
+    if (isConnected) return console.log('==> already connected');
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+
+
+
+
+
+
+
+        isConnected = true;
+        console.log('==> connected to mongodb');
+
+    } catch (error) {
+        console.log(error);
+    }
+
+}
